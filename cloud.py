a2ca6ab6f5d6a489a04e2f14d82e949a9d6649b7
Craig Steele <cr@igsteele.com>
2019-04-03 22:39:11 +0100

    Update to work with new Yahoo Weather API

--- cloud.py
+++ cloud.py
@@ -1,4 +1,5 @@
-from weather import Weather, Unit
+from yahoo_weather import yahoo_weather
+from yahoo_weather.config.units import Unit
 import pgzrun
 import os
 import spotify_app
@@ -8,15 +9,24 @@ WIDTH, HEIGHT = 400, 400
 print('Sound Cloud App')
 spotify_app.spotify_setup()
 
-weather = Weather(unit=Unit.CELSIUS)
-location = weather.lookup_by_location('Perth, WA')
-weather_condition = location.condition
+
+weather = yahoo_weather.YahooWeather(
+        APP_ID="DIpksy4o",
+        apikey="dj0yJmk9VUc1S3dWQndyYzVmJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTUz",
+        apisecret="63d81895f2ac41afcb5a1db552a74714bf1e00f2")
+
+weather.get_yahoo_weather_by_city("Glasgow", Unit.celsius)
+print(weather.condition.text)
+print(weather.condition.temperature)
+
+weather_condition = weather.condition
 spotify_app.play_track_for_weather(weather_condition)
 
 
 def draw():
     screen.clear()
     screen.fill('blue')
-    screen.draw.text(weather_condition.text, (20, 100))
+    screen.draw.text('Sound Cloud App', (0, 0))
+    screen.draw.text(weather_condition.text, (100, 100))
 
 pgzrun.go()
