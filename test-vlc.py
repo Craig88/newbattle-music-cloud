526c824194bcde6e4061a455eb5b2adf656b5992
Craig Steele <cr@igsteele.com>
2019-04-07 14:51:35 +0100

    flake8 style fixes

--- test-vlc.py
+++ test-vlc.py
@@ -1,4 +1,5 @@
-import vlc, time
+import vlc
+import time
 
 # Sample of Chvrches song
 mp3_url = "https://p.scdn.co/mp3-preview/9fbeaa5db69e171ed646374d1da92bdde74044ad?cid=f4e4247d61054c97aab361f9f95a06e0"
@@ -10,7 +11,7 @@ print(media.get_state())
 
 # A bit of a cheat here to allow it to play
 while True:
-     pass
-     time.sleep(1)
-     print("Checking state..")
-     print(media.get_state())
+    pass
+    time.sleep(1)
+    print("Checking state..")
+    print(media.get_state())
