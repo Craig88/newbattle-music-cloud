dd622dbbfd787ed537697de8b5c769294174adde
Craig Steele <cr@igsteele.com>
2019-04-07 14:46:03 +0100

    Updating weather test to use env variables

--- test-weather.py
+++ test-weather.py
@@ -1,11 +1,22 @@
+import os
 from yahoo_weather.weather import YahooWeather
 from yahoo_weather.config.units import Unit
+from dotenv import load_dotenv
+
+load_dotenv()
+
+id = os.getenv('YAHOO_APP_ID')
+key = os.getenv('YAHOO_API_KEY')
+secret = os.getenv('YAHOO_API_SECRET')
+
+city_name = "Glasgow"
 
 weather = YahooWeather(
-        APP_ID="***REMOVED***",
-        api_key="***REMOVED***",
-        api_secret="***REMOVED***")
+        APP_ID=id,
+        api_key=key,
+        api_secret=secret)
 
-weather.get_yahoo_weather_by_city("Glasgow", Unit.celsius)
+weather.get_yahoo_weather_by_city(city_name, Unit.celsius)
+print(city_name)
 print(weather.condition.text)
 print(weather.condition.temperature)
