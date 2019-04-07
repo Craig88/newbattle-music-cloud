64e564af998923c907859c049ff9f5ae88fabcca
Craig Steele <cr@igsteele.com>
2019-04-07 14:45:25 +0100

    Updating VLC test to use stream

--- test-vlc.py
+++ test-vlc.py
@@ -1,7 +1,16 @@
-import vlc
-media = vlc.MediaPlayer("audio.mp3")
+import vlc, time
+
+# Sample of Chvrches song
+mp3_url = "https://p.scdn.co/mp3-preview/9fbeaa5db69e171ed646374d1da92bdde74044ad?cid=f4e4247d61054c97aab361f9f95a06e0"
+
+media = vlc.MediaPlayer(mp3_url)
 media.play()
+
+print(media.get_state())
 
 # A bit of a cheat here to allow it to play
 while True:
      pass
+     time.sleep(1)
+     print("Checking state..")
+     print(media.get_state())
