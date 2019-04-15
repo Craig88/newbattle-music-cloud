518870e7cbb9ae23a1bd0688896a8b55994cb788
Craig Steele <cr@igsteele.com>
2019-04-15 21:19:46 +0100

    Fix typo

--- test_vlc.py
+++ test_vlc.py
@@ -2,7 +2,7 @@ import vlc
 import time
 import sys
 
-print("Testing VLC player..."")
+print("Testing VLC player...")
 
 # Sample of Chvrches song
 mp3_url = "https://p.scdn.co/mp3-preview/9fbeaa5db69e171ed646374d1da92bdde74044ad?cid=***REMOVED***"
