e310f45786697dc98ed4c44ca4b2b332ad7a9803
Craig Steele <cr@igsteele.com>
2019-04-15 21:18:16 +0100

    Updating VLC test

--- test_vlc.py
+++ test_vlc.py
@@ -1,7 +1,8 @@
 import vlc
 import time
+import sys
 
-print('Testing VLC player...')
+print("Testing VLC player..."")
 
 # Sample of Chvrches song
 mp3_url = "https://p.scdn.co/mp3-preview/9fbeaa5db69e171ed646374d1da92bdde74044ad?cid=***REMOVED***"
@@ -15,5 +16,8 @@ print(media.get_state())
 while True:
     pass
     time.sleep(1)
-    print("Checking state..")
-    print(media.get_state())
+    # print("Checking state..")
+    # print(media.get_state())
+    if (str(media.get_state()) == "State.Ended"):
+        print("This is finished now")
+        sys.exit()
