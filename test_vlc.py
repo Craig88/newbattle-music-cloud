4170347fa95617a73e39cc75b17d8140a3cf2a3b
Craig Steele <cr@igsteele.com>
2019-04-16 22:02:27 +0100

    Update spotify to stop program after stream

--- test_vlc.py
+++ test_vlc.py
@@ -4,7 +4,7 @@ import sys
 
 print("Testing VLC player...")
 
-# Sample of Chvrches song
+# Sample of a Chvrches song
 mp3_url = "https://p.scdn.co/mp3-preview/9fbeaa5db69e171ed646374d1da92bdde74044ad?cid=f4e4247d61054c97aab361f9f95a06e0"
 
 media = vlc.MediaPlayer(mp3_url)
