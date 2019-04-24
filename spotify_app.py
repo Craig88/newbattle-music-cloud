ba913f4586b25029556974e4cffce6603e61b151
Craig Steele <cr@igsteele.com>
2019-04-24 20:54:37 +0100

    Updating spotify app

--- spotify_app.py
+++ spotify_app.py
@@ -4,23 +4,24 @@ import os
 import vlc
 import time
 from dotenv import load_dotenv
-from sys import exit
-
-debug_mode = True
 
 load_dotenv()
+
 
 username = os.getenv('SPOTIFY_USERNAME')
 my_id = os.getenv('SPOTIFY_CLIENT_ID')
 my_secret = os.getenv('SPOTIFY_CLIENT_SECRET')
 scope = 'user-library-read'
 
-umbrella = 'spotify:track:5i66xrvSh1MjjyDd6zcwgj'
+showers_track = 'spotify:track:5i66xrvSh1MjjyDd6zcwgj'
 sunny_track = 'spotify:track:3pf96IFggfQuT6Gafqx2rt'
 cloudy_track = 'spotify:track:5icOoE6VgqFKohjWWNp0Ac'
 
+debug_mode = False  # set to True to turn on a debugging mode
+
 
 def spotify_setup():
+    """ connects to Spotify using the API """
     token = util.prompt_for_user_token(username, scope,
                                        client_id=my_id,
                                        client_secret=my_secret,
@@ -36,19 +37,31 @@ def play_track_for_weather(weather):
     print("This plays the track for the weather")
     print(weather.text)
 
-    if weather.text == "Partly Cloudy" or (debug_mode):
-        print("We need to play a cloudy track")
+    if weather.text == "Sunny" or (debug_mode):
+        print("We need to play a Sunny track")
         track = sp.track(sunny_track)
         preview_track = track['preview_url']
         print(preview_track)
         track = vlc.MediaPlayer(preview_track)
         track.play()
 
-        while True:
-            pass
-            time.sleep(1)
-            print("Checking state..")
-            print(track.get_state())
-            if (str(track.get_state()) == "State.Ended"):
-                print("This is finished now")
-                exit()
+    elif weather.text == "Showers":
+        print("We need to play a Showers track")
+        track = sp.track(showers_track)
+        preview_track = track['preview_url']
+        print(preview_track)
+        track = vlc.MediaPlayer(preview_track)
+        track.play()
+
+    else:
+        print("Not a recognised weather condition")
+        return
+
+    while True:
+        time.sleep(1)
+        print("Checking state..")
+        print(track.get_state())
+        if (str(track.get_state()) == "State.Ended"):
+            print("This track has finished now")
+            # exit()
+            return
