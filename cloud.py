2ea261cf9e9fc9a25501235b020b52d5b6989733
Craig Steele <cr@igsteele.com>
2019-04-24 20:55:26 +0100

    Updating main app

--- cloud.py
+++ cloud.py
@@ -1,18 +1,23 @@
 from yahoo_weather.weather import YahooWeather
 from yahoo_weather.config.units import Unit
-import time
 import spotify_app
 import weather_app
 
-WIDTH, HEIGHT = 400, 400
-
-print("Sound Cloud App started")
+print("Newbattle Sound Cloud App started")
+# Set up Spotify and Weather
 spotify_app.spotify_setup()
 weather = weather_app.weather_setup()
 
+# Set the weather for the city
 weather.get_yahoo_weather_by_city("Glasgow", Unit.celsius)
-print(weather.condition.text)
+print("The city is set to %s" % weather.location.city)
+print("The weather conditions are currently %s" % weather.condition.text)
 print(weather.condition.temperature)
+print("Sunrise is at: %s" % weather.current_observation.astronomy.sunrise)
+print("Sunset is at: %s" % weather.current_observation.astronomy.sunset)
 
-weather_condition = weather.condition
-spotify_app.play_track_for_weather(weather_condition)
+# Use Spotify to play a track based on the weather conditions
+spotify_app.play_track_for_weather(weather.condition)
+
+# End the program
+print("Newbattle Sound Cloud App finished")
