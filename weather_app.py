3a1ae0afcf7a880063dbcd0ab0bbf665eb04ce35
Craig Steele <cr@igsteele.com>
2019-04-29 15:31:26 +0100

    Improve internal commentary

--- weather_app.py
+++ weather_app.py
@@ -1,13 +1,14 @@
 import os
 from yahoo_weather.weather import YahooWeather
 
-
+# Get the API credentials, these were loaded in from the .env file
 my_id = os.getenv('YAHOO_APP_ID')
 my_key = os.getenv('YAHOO_API_KEY')
 my_secret = os.getenv('YAHOO_API_SECRET')
 
 
 def weather_setup():
+    """ Connects to the Yahoo Weather API. """
     weather = YahooWeather(
             APP_ID=my_id,
             api_key=my_key,
