f5521706af80329b3cd0426d7e9c3143d9cb892d
Craig Steele <cr@igsteele.com>
2019-04-29 10:54:42 +0100

    Tidy up style

--- cloud.py
+++ cloud.py
@@ -12,9 +12,11 @@ weather = weather_app.weather_setup()
 weather.get_yahoo_weather_by_city("Glasgow", Unit.celsius)
 print("The city is set to %s" % weather.location.city)
 print("The weather conditions are currently %s" % weather.condition.text)
-print(weather.condition.temperature)
+print("The weather status code is %s" % weather.condition.code)
+print("The temperature is %s" % weather.condition.temperature)
 print("Sunrise is at: %s" % weather.current_observation.astronomy.sunrise)
 print("Sunset is at: %s" % weather.current_observation.astronomy.sunset)
+
 
 # Use Spotify to play a track based on the weather conditions
 spotify_app.play_track_for_weather(weather.condition)
